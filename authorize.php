e1e4f22e533c2fc5c98037e4a717a9b290f812ac
grestorv <vertoserk@yandex.by>
2020-07-08 22:31:01 +0300

    Версия 1.0

---
+++ authorize.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+function getToken($authData)
+{
+
+    $data = [
+        'client_id' => $authData['client_id'],
+        'client_secret' => $authData['client_secret'],
+        'grant_type' => $authData['grant_type'],
+        'code' => $authData['code'],
+        'redirect_uri' => $authData['redirect_uri'],
+
+    ];
+
+    /**
+     * Нам необходимо инициировать запрос к серверу.
+     * Воспользуемся библиотекой cURL (поставляется в составе PHP).
+     * Вы также можете использовать и кроссплатформенную программу cURL, если вы не программируете на PHP.
+     */
+    $curl = curl_init(); //Сохраняем дескриптор сеанса cURL
+    /** Устанавливаем необходимые опции для сеанса cURL  */
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-oAuth-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, 'https://'.$authData['baseDomain'].'/oauth2/access_token');
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+    $out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+    /** Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+    $code = (int)$code;
+    $errors = [
+        400 => 'Bad request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not found',
+        500 => 'Internal server error',
+        502 => 'Bad gateway',
+        503 => 'Service unavailable',
+    ];
+
+    try {
+        /** Если код ответа не успешный - возвращаем сообщение об ошибке  */
+        if ($code < 200 || $code > 204) {
+            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undefined error', $code);
+        }
+    } catch (Exception $e) {
+        die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
+    }
+
+    /**
+     * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+     * нам придётся перевести ответ в формат, понятный PHP
+     */
+    $response = json_decode($out, true);
+
+
+    file_put_contents(TOKEN_FILE, json_encode($response));
+    return $response;
+}
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+    require_once 'vendor/autoload.php';
+    use AmoCRM\Client\AmoCRMApiClient;
+    use AmoCRM\Exceptions\AmoCRMApiException;
+    use AmoCRM\Exceptions\AmoCRMApiNoContentException;
+    use AmoCRM\Exceptions\AmoCRMoAuthApiException;
+    use AmoCRM\Helpers\EntityTypesInterface;
+    use League\OAuth2\Client\Token\AccessToken;
+    use AmoCRM\Collections\TasksCollection;
+    use AmoCRM\Models\TaskModel;
+
+    define('DATA_FILE', 'data_info.json');
+    define('TOKEN_FILE', 'token_info.json');
+
+    require_once "authorize.php";
+
+    //Проверяем наличие файлов с аутентификационными данными
+    if ((!file_exists(DATA_FILE)) OR (!file_exists(TOKEN_FILE))) {
+        exit('Data file or Token file not found');
+    }
+    //Считываем эти данные в массив
+    $authData = json_decode(file_get_contents(DATA_FILE), true);
+    //Валидируем данные
+    if (!(
+        isset($authData)
+        && isset($authData['client_id'])
+        && isset($authData['client_secret'])
+        && isset($authData['grant_type'])
+        && isset($authData['code'])
+        && isset($authData['redirect_uri'])
+        && isset($authData['baseDomain'])
+    )){
+        die("Установите данные для аутентификации в файле data_info.json");
+    }
+    //Считываем токены в массив.
+    $accessToken = json_decode(file_get_contents(TOKEN_FILE), true);
+    if (!(
+        isset($accessToken)
+        && isset($accessToken['access_token'])
+        && isset($accessToken['refresh_token'])
+        && isset($accessToken['expires_in'])
+    )){
+        $accessToken = getToken($authData);
+        //$accessToken = json_decode(file_get_contents(TOKEN_FILE), true);
+    }
+    $apiClient = new AmoCRMApiClient($authData['client_id'], $authData['client_secret'], $authData['redirect_uri']);
+    $apiClient->setAccountBaseDomain($authData['baseDomain']);
+
+    //устанавливаем токены нашему соединению
+    $apiClient->setAccessToken(new AccessToken([
+    'access_token' => $accessToken['access_token'],
+    'refresh_token' => $accessToken['refresh_token'],
+    'expires' => $accessToken['expires_in'],
+    'baseDomain' => $authData['baseDomain'],
+]));;
+
+    //Получаем список сделок в виде массива
+    try {
+        $leadsArr=$apiClient->leads()->get()->all();
+    } catch (AmoCRMoAuthApiException $e) {
+        //Если произошла ошибка аутентификации - возможно токен устарел, пробуем получить новый
+        //$accessToken = getToken($authData);
+        die("Неверные данные для аутентификации. Попробуйте изменить их и перезагрузиться. $e");
+    } catch (AmoCRMApiException $e) {
+        die($e);
+    }
+    //Получаем список задач в виде массива
+    try {
+        $tasksArr= $apiClient->tasks()->get()->all();
+    } catch (AmoCRMApiNoContentException $e) {
+
+    } catch (AmoCRMApiException $e) {
+        die($e);
+    }
+    //Переделываем массив сделок так, чтобы индексом являлся id сделки
+    if(isset($leadsArr)){
+        foreach ($leadsArr as $key=>$lead){
+            $leadsArr[$lead->getId()]=$lead;
+            unset($leadsArr[$key]);
+        }
+    }
+    //Переделываем массив задач так, чтобы индексом являлся id сущности, на которую ссылается задача
+    if(isset($tasksArr)){
+        foreach ($tasksArr as $key=>$task){
+            $tasksArr[$task->getEntityId()]=$task;
+            unset($tasksArr[$key]);
+        }
+        $leadsArr=array_diff_key($leadsArr, $tasksArr);//Удаляем из массива сделок все сделки, id которых совпадает с ententityId задачи
+    }
+    //Добавляем задачи в коллекцию
+    $tasksCollection=new TasksCollection();
+    foreach ($leadsArr as $lead){
+        $task = new TaskModel();;
+        $task->setTaskTypeId(TaskModel::TASK_TYPE_ID_CALL)
+            ->setText('Сделка без задачи')
+            ->setCompleteTill(mktime(10, 0, 0, 8, 10, 2020))
+            ->setEntityType(EntityTypesInterface::LEADS)
+            ->setEntityId($lead->getId())
+            ->setDuration(30 * 60 * 60) //30 минут
+            ->setResponsibleUserId($lead->getResponsibleUserId());
+            $tasksCollection->add($task);
+    }
+    //Если коллекция задач не пуста, добавляем ее в список задач
+    if(!$tasksCollection->isEmpty()) {
+        $tasksCollection = $apiClient->tasks()->add($tasksCollection);
+        echo "Задачи добавлены";
+    }
+    else {
+        echo "У всех сделок есть задачи";
+    }
